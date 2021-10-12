eee0d2d6f6f9b167cad4ece36753919f22078865
Cristian Prochnow <cristianprochnow@gmail.com>
2021-10-12 17:51:16 -0300

    feat: add dinamic content for posts

--- src/script.js
+++ src/script.js
@@ -1,5 +1,27 @@
-import jsonData from '../assets/data.json';
+import { data } from './services/server.js';
 
-const data = JSON.parse(jsonData);
+const contentSection = document.querySelector('.blog-content .page-wrapper');
+const htmlContent = data.map(post => getPost(post));
+const htmlText = htmlContent.join('\n');
 
-console.log(data);
+contentSection.innerHTML = htmlText;
+
+/**
+ * @param {typeof data[0]} data
+ * @returns string
+ */
+function getPost(data) {
+  return `
+    <article class="post">
+      <header>
+        <span>${data.date}</span>
+        <img src="./assets/heart.svg" alt="Favorite icon">
+      </header>
+
+      <main>
+        <h1>${data.title}</h1>
+        <p>${data.text}</p>
+      </main>
+    </article>
+  `;
+}
